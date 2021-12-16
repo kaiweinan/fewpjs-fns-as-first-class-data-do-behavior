00391e282aa8e3998e2308ed4a2ba280258fa4e1
kaiweinan <kaiweinan@gmail.com>
2021-12-16 18:09:31 -0500

    commit tree 37681ff5c86319d651310c3c8ecce3142c4d7687

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(m) {
+  document.getElemntsById("greeting").context = m;
+  
+}
